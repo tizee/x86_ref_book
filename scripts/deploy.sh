8053809080772ce00e7e8b8ee3c488ff3a00d1ea
Tizee <tizee@qq.com>
2019-03-17 01:24:58 +0800

    :hammer: fix travis,deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -ev
 
 # setup git
 git config --global user.name tizee-bot
-git config --global user.email pobomp@gmail.com
+git config --global user.email nobody@nobody.org
 git config --global push.default simple
 
 # navigate to build output directory
@@ -13,9 +13,10 @@ cd docs/.vuepress/dist
 
 git init
 git add --all
-git commit -m ':books: auto deploy'
+git commit -m ':books: auto deploy' > /dev/null 2>&1
+# push 
+git push -f -q "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" HEAD:master > /dev/null 2>&1
+
 # list remote
 git remote -v
-# push 
-git push -f -q "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" HEAD:master > /dev/null 2>&1
 
