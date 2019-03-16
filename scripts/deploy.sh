c703ed62a40b3be9cc3526280f4e768dda0b4784
Tizee <tizee@qq.com>
2019-03-16 23:57:55 +0800

    fix deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+#npm run build
+
+git config --global user.name tizee-bot
+git config --global user.email pobomp@gmail.com
+git config --global push.default simple
+
+# navigate to build output directory
+cd docs/.vuepress/dist
+
+git init
+git add --all
+git commit -m ':books: auto deploy'
+# push 
+git push -q "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" HEAD:master
+
