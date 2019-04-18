87eab1e0e7677fff131d9cc76b97f4106d69187b
Tizee <tizee@qq.com>
2019-04-18 12:44:19 +0800

    :hammer: update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,8 +57,8 @@ module.exports = {
             }
         ],
         sidebar: {
-            '/instruction/': getInstrSidebar(),
-	    '/':['']
+		'/instruction/':getInstrSidebar(),
+		'/':['']
         }
     },
     //=== Markdown,
